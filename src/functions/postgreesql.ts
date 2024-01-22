6a662fc5bb9c3735ce2e936c3ba012570b03d6e3
Cyb3rWolf945 <59710384+Cyb3rWolf945@users.noreply.github.com>
2024-01-22 17:41:42 +0000

    update
    update cors

--- src/functions/postgreesql.ts
+++ src/functions/postgreesql.ts
@@ -41,3 +41,53 @@ class PostgresConnection {
 }
 
 export default PostgresConnection;
+
+
+// import * as mysql from 'mysql2/promise';
+
+// const mysqlConfig = {
+//   host: '193.137.5.175',
+//   user: 'paulodaniel',
+//   password: 'x4x6x10',
+//   database: '',
+// };
+
+// class MySqlConnection {
+//   private connection: mysql.Connection;
+
+//   constructor() {
+//     this.connection = mysql.createConnection(mysqlConfig);
+//   }
+
+//   async connect(): Promise<void> {
+//     try {
+//       await this.connection.connect();
+//       console.log('Connected to MySQL database');
+//     } catch (error) {
+//       console.error('Error connecting to MySQL database:', error);
+//       throw error; // Propagate the error for handling in the calling code
+//     }
+//   }
+
+//   async disconnect(): Promise<void> {
+//     try {
+//       await this.connection.end();
+//       console.log('Connection to MySQL closed');
+//     } catch (error) {
+//       console.error('Error closing connection to MySQL database:', error);
+//       throw error;
+//     }
+//   }
+
+//   async query(sql: string, params?: any[]): Promise<[any, mysql.FieldPacket[]]> {
+//     try {
+//       const result = await this.connection.execute(sql, params);
+//       return result;
+//     } catch (error) {
+//       console.error('Error executing query:', error);
+//       throw error;
+//     }
+//   }
+// }
+
+// export default MySqlConnection;
